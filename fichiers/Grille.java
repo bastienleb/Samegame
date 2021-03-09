fd81df2be7ae54481a5382eba0ec00c2d9b39f56
metri <kevin.metri@etu.u-pec.fr>
2021-03-09 15:50:13 +0100

    _

---
+++ fichiers/Grille.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+/*
+@author Kévin METRI et Bastien LEBLET
+*/
+
+public class Grille {
+    private char tab[][]=new char[10][15];
+    private Random choix=new Random();
+    public Grille(){
+        for (int i=0;i<10;i++){
+            for(int j=0;j<15;j++){
+                int hasard=choix.nextInt(3);
+
+                if(hasard==0){
+                    tab[i][j]='R';
+                    System.out.print(tab[i][j]+" ");
+                }
+
+                if(hasard==1){
+                    tab[i][j]='B';
+                     System.out.print(tab[i][j]+" ");
+                }
+
+                if(hasard==2){
+                    tab[i][j]='V';
+                    System.out.print(tab[i][j]+" ");
+                }
+                
+            }
+            System.out.println("");
+ 
+        }
+        
+    
+
+    // for (int i=0;i<10;i++){
+    //     System.out.println("");
+    //     for(int j=0;j<15;j++){
+    //         System.out.print(tab[i][j]+" ");
+    //     }
+    // }
+
+}
+
+}
