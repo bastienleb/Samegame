34115a1792d90baa5bf71e085f670edd9cc59140
leblet <bastien.leblet@etu.u-pec.fr>
2021-03-11 12:08:27 +0100

    Menu

--- fichiers/Menu.java
+++ fichiers/Menu.java
@@ -8,21 +8,23 @@ import java.awt.*;
 public class Menu extends JFrame {
 
     JPanel panneau = new JPanel();
+    int valeur;
     Actionmenu m = new Actionmenu(panneau);
     
-
-    public Menu(){
+    
+    public Menu(int tmp){
         super("SameGame");
         this.setSize(1125,750);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        System.out.println("dans le menu");
+        System.out.println("dans le menu "+valeur);
 
         JButton aleatoire = new JButton("Al\u00e9atoire");
         aleatoire.addActionListener(m);
         panneau.add(aleatoire);
+
         JButton bouton2 = new JButton("D\u00e9fault");
         panneau.add(bouton2);
         bouton2.addActionListener(m);
@@ -30,14 +32,16 @@ public class Menu extends JFrame {
         JButton bouton3 = new JButton("Quitter");
         panneau.add(bouton3);
         bouton3.addActionListener(m);
+
+        this.valeur=tmp;
+        
+        if(valeur == 1){
+            this.dispose();
+            System.out.println("c fermer");
+        }
+        
         
         this.add(panneau);
         this.setVisible(true);
-    }
-
-    public void fermer(){
-        System.out.println("dans la fonction qui ferme");
-        this.dispose();
-    }
-   
+    }   
 }
