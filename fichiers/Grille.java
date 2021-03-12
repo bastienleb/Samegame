a3474a709dce0369eef68f216f0ea7e50655b361
leblet <bastien.leblet@etu.u-pec.fr>
2021-03-12 18:19:51 +0100

    Souris sa marche on est trop fort

--- fichiers/Grille.java
+++ fichiers/Grille.java
@@ -1,18 +1,20 @@
 import java.util.*;
-import java.util.function.Supplier;
-
 import javax.swing.*;
+import java.awt.event.*;
+
 import java.awt.*;
 /*
 @author Kévin METRI et Bastien LEBLET
 */
 
-public class Grille extends JFrame {
+public class Grille extends JFrame implements MouseMotionListener {
     private char tab[][]=new char[10][15];
     private Random choix=new Random();
     
     public Grille(){
         super("Grille");
+        this.addMouseMotionListener(this);
+
         this.setSize(1125,750);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
@@ -61,7 +63,6 @@ public class Grille extends JFrame {
             fenetre.setLayout(new GridLayout(15,10));
             
             JPanel panel =new JPanel();
-            panel.setBackground(Color.BLACK);
             
             panel.setLayout(new GridLayout(10,15));
             
@@ -69,33 +70,52 @@ public class Grille extends JFrame {
                 for(int j=0;j<15;j++){
                     this.add(panel);
                     String lettre=Character.toString(tab[i][j]);
-                    //System.out.println(lettre);
                     
                     if(lettre.equals("R")){
                         panel.add(new Image(new ImageIcon("../image/rouge.png").getImage()));
-                        
-                        System.out.print("R");
+                        //System.out.print("R");
 
                     }
                     
                     if(lettre.equals("V")){
                         panel.add(new Image(new ImageIcon("../image/vert.png").getImage()));
-                        System.out.print("V");
+                        //System.out.print("V");
                         
                     }
                     
                     if(lettre.equals("B")){
                         panel.add(new Image(new ImageIcon("../image/bleu.png").getImage()));
-                        System.out.print("B");
+                        //System.out.print("B");
 
                     }
                     
                 }
-                System.out.println("");
+                //System.out.println("");
             }
             
             this.setVisible(true);
         }
+
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+        //this.repaint();
+        
+    }
+
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        //System.out.println("x="+e.getX()+" y="+e.getY());
+        if((e.getX()>=0 && e.getX()<=100)&&(e.getY()>=0 && e.getY()<=100)){
+            System.out.println("patate");
+        }
+        //Graphics g=getGraphics();  
+        //g.setColor(Color.BLUE);  
+        //g.fillOval(e.getX(),e.getY(),20,20);
+    }
         
     }
     
