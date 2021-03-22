94a6985b97915e97109e71750d928d8ca0fa4b0c
leblet <bastien.leblet@etu.u-pec.fr>
2021-03-22 17:12:14 +0100

    ON A LES FICHIER QUI MARCHE ET C'EST QUE DES TXT MGL

--- fichiers/RecupFichier.java
+++ fichiers/RecupFichier.java
@@ -1,12 +1,14 @@
 import java.io.*;
-import javax.swing.JFileChooser;
-import javax.swing.filechooser.FileSystemView;
+import javax.swing.*;
+import javax.swing.filechooser.*;
 
 public class RecupFichier {
 
     public String recuperation() {
         JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-
+        jfc.setAcceptAllFileFilterUsed(false);
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(".txt", "txt");
+        jfc.addChoosableFileFilter(filter);
         int returnValue = jfc.showOpenDialog(null);
         String chemin="a";
 
