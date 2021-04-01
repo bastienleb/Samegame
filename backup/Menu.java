7957b554c5a96d579ce2e146c3a11c6a0cef8d17
leblet <bastien.leblet@etu.u-pec.fr>
2021-04-01 08:53:27 +0200

    -

--- backup/Menu.java
+++ backup/Menu.java
@@ -9,29 +9,25 @@ import java.awt.*;
 public class Menu extends JFrame {
 
     JPanel panneau = new JPanel();
-    JPanel titre = new JPanel();
-    JPanel logo = new JPanel();
-    JLabel nomjeu = new JLabel("SameGame");
-    Actionmenu m = new Actionmenu(panneau,this);
+    ImageIcon icone = new ImageIcon("../image/image du menu.jpg");
+    JLabel images = new JLabel(icone);
     
+    Actionmenu m = new Actionmenu(panneau,this);
     
     public Menu(){
         super("SameGame");
-        this.setSize(750,750);
+        this.setSize(1024,768);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        GridLayout grillage = new GridLayout(3,1);
-        this.setLayout(grillage);
-
         JButton aleatoire = new JButton("Al\u00e9atoire");
         aleatoire.addActionListener(m);
         aleatoire.setFont(new Font("Sérif",Font.BOLD,20));
         aleatoire.setBackground(new Color(186,22,28));
         aleatoire.setBorder(BorderFactory.createLineBorder(Color.BLACK,2));
         aleatoire.setForeground(Color.BLACK);
-        panneau.add(aleatoire);
+        //panneau.add(aleatoire);
 
 
         JButton fichier = new JButton("Choix fichier");
@@ -40,7 +36,7 @@ public class Menu extends JFrame {
         fichier.setBackground(new Color(0,168,81));
         fichier.setBorder(BorderFactory.createLineBorder(Color.BLACK,2));
         fichier.setForeground(Color.BLACK);
-        panneau.add(fichier);
+        //panneau.add(fichier);
 
         JButton quiter = new JButton("Quitter");
         quiter.addActionListener(m);
@@ -48,24 +44,13 @@ public class Menu extends JFrame {
         quiter.setBackground(new Color(2,176,241));
         quiter.setBorder(BorderFactory.createLineBorder(Color.BLACK,2));
         quiter.setForeground(Color.BLACK);        
-        panneau.add(quiter);
+        //panneau.add(quiter);
 
-        nomjeu.setFont(new Font("Sérif",Font.BOLD,45));
-        titre.add(nomjeu);
+        panneau.add(images);
 
-
-        ImageIcon icone = new ImageIcon("../image/jeu.png");
-        JLabel jlabel = new JLabel(icone, JLabel.CENTER);
-        logo.add(jlabel);
-
-    
-        titre.setBackground(new Color(0,128,128));
-        panneau.setBackground(new Color(0,128,128));
-        logo.setBackground(new Color(0,128,128));
-
-        this.add(titre);
+        //this.add(titre);
         this.add(panneau);
-        this.add(logo);
+        //this.add(logo);
         this.setVisible(true);
     }   
 }
